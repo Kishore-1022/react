25b3a115273da0a50aa0758a46e6d296dabbe040
Kishore-1022 <kishoresmart229@gmail.com>
2023-09-27 11:26:24 +0530

    git

---
+++ krish/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExpenseItems from './components/ExpenseItems';
+
+function App() {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      <ExpenseItems></ExpenseItems>
+      
+    </div>
+  );
+}
+
+export default App;
