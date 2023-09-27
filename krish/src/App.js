405b9d0a6619a841947add4422aa165267bba77d
Kishore-1022 <kishoresmart229@gmail.com>
2023-09-27 16:42:34 +0530

    making components smaller and readable

--- krish/src/App.js
+++ krish/src/App.js
@@ -8,18 +8,21 @@ function App() {
     title:'car insurance',
     date:new Date(2020, 7, 12),
     amount:1000,
+    location:"Chennai"
    },
    {
     id:2,
     title:'house exp',
     date:new Date(2021, 2, 29),
     amount:2000,
+    location:"Delhi"
    },
    {
     id:3,
     title:'phone exp',
     date:new Date(2022, 4, 19),
     amount:1500,
+    location:"mumbai"
    }
   ]
   return (
@@ -31,32 +34,14 @@ function App() {
          title={i.title}
          amount={i.amount}
          date={i.date}
+         location={i.location}
          />
       ))}</div>
-      {/* // <ExpenseItems  */}
-      
-      {/* //   title={i.title}
-      //   amount={i.amount}
-      //   date={i.date}))}/> */}
-
-      {/* <ExpenseItems
-       title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}>
-        </ExpenseItems>
-      <ExpenseItems 
-      title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date}>
-      </ExpenseItems>
-      <ExpenseItems 
-      title={expenses[2].title}
-      amount={expenses[2].amount}
-      date={expenses[2].date}>
-      </ExpenseItems>
-      <ExpenseItems /> */}
-     
-      
+      {/* <ExpenseItems 
+      title={expenses[0].title}
+      date={expenses[0].date}
+      amount={expenses[0].amount}
+      /> */}
       
     </div>
   );
--- krish/src/components/ExpenseItems.js
+++ krish/src/components/ExpenseItems.js
@@ -1,11 +1,17 @@
 import './ExpenseItems.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItems(props){
   return (
     <div className='expense-item'>
-         <div>{props.date.toLocaleString()}</div>
+      <div>
+        <ExpenseDate date={props.date} />
+        
+      </div>
+
          <div className='expense-item__description'>
            <h2> {props.title}</h2>
+           <h2>{props.location}</h2>
          </div>
          <div className='expense-item__price'>Rs.{props.amount}</div>
          
