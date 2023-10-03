60403080f7207bdd7d6c8ee3470797a3a82a9bd5
Kishore-1022 <kishoresmart229@gmail.com>
2023-10-03 20:16:55 +0530

    dynamic

--- krish/src/App.js
+++ krish/src/App.js
@@ -1,40 +1,42 @@
 import ExpenseItems from "./components/ExpenseItems"
 import NewExpenses from "./components/NewExpenses/NewExpenses";
+import { useState } from "react";
 
-
+const dummyExpenses=[
+  {
+  id:1,
+  title:'car insurance',
+  date:new Date(2020, 7, 12),
+  amount:1000,
+  location:"Chennai"
+ },
+ {
+  id:2,
+  title:'house exp',
+  date:new Date(2021, 2, 29),
+  amount:2000,
+  location:"Delhi"
+ },
+ {
+  id:3,
+  title:'phone exp',
+  date:new Date(2022, 4, 19),
+  amount:1500,
+  location:"mumbai"
+ }
+]
 const App=()=>{
-  const expenses=[
-    {
-    id:1,
-    title:'car insurance',
-    date:new Date(2020, 7, 12),
-    amount:1000,
-    location:"Chennai"
-   },
-   {
-    id:2,
-    title:'house exp',
-    date:new Date(2021, 2, 29),
-    amount:2000,
-    location:"Delhi"
-   },
-   {
-    id:3,
-    title:'phone exp',
-    date:new Date(2022, 4, 19),
-    amount:1500,
-    location:"mumbai"
-   }
-  ]
+  const [expenses,setExpenses]=useState(dummyExpenses);
+   
   const addExpenseHandler=expense=>{
-    console.log('kishore')
-    console.log(expense)
+    setExpenses([expense, ...expenses])
+ 
   }
   return (
     <div>
       
       <NewExpenses  onAddExpense={addExpenseHandler}/>
-      
+
       <div>{expenses.map(i=>(
          <ExpenseItems 
          id={i.id}
